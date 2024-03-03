7472aa5e5681c560523141efeeaa812bc9395380
Sim_out <alik2006sootkill@gmail.com>
2024-03-04 00:41:23 +0700

    player walk update

--- Assets/Script/Player/PlayerWalk.cs
+++ Assets/Script/Player/PlayerWalk.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 public class PlayerWalk : MonoBehaviour
 {
     [SerializeField] private float Speed = 1f;
-
+    private float _movespeedmultiplier = 1;
     private Rigidbody _rb;
-
     private PlayerInput _playerinput;
+    private void HandeIsGround(bool flag)
+    {
+        if (flag)
+            _movespeedmultiplier = 1;
+        else
+            _movespeedmultiplier = 0.5f;
+    }
     private void Awake()
     {
         _playerinput = GetComponent<PlayerInput>();
         _rb = GetComponent<Rigidbody>();
+        PlayerCheckGround.IsGround.AddListener(HandeIsGround);
     }
+    
     private void Update()
     {
-        Vector3 velocity = new Vector3(_playerinput.Horizontal, 0, 0) * Speed /** _movespeedmultiplier*/;
+        Vector3 velocity = new Vector3(_playerinput.Horizontal, 0, 0) * Speed * _movespeedmultiplier;
         velocity.y = _rb.velocity.y;
         Vector3 worldVelocity = transform.TransformVector(velocity);
         _rb.velocity = worldVelocity;
