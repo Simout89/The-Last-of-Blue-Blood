a8e59f1ae443743af989352f731f6006eb864bdd
Sim_out <alik2006sootkill@gmail.com>
2024-03-04 13:27:05 +0700

    update goblin

--- Assets/Script/Enemy/Goblin/GoblinBerserk.cs
+++ Assets/Script/Enemy/Goblin/GoblinBerserk.cs
@@ -8,8 +8,6 @@ public class GoblinBerserk : MonoBehaviour
     [SerializeField] private float Speed = 1f;
     private GoblinInput _goblininput;
     public static UnityEvent OnBerserk = new UnityEvent();
-    private bool _rotate = true; // false - left , true - right
-    private bool _newrotate = false;
     private void Awake()
     {
         _goblininput = GetComponent<GoblinInput>();
@@ -18,31 +16,9 @@ public class GoblinBerserk : MonoBehaviour
 
     private void HandleBerserk()
     {
-        Rotate();
+        GoblinBerserkRotate.OnBerserkRotate.Invoke();
         Vector3 direction = (_goblininput.Player.transform.position - _goblininput.GoblinBody.transform.position).normalized;
         direction.y = 0;
         _goblininput.GoblinRigidbody.MovePosition(_goblininput.GoblinRigidbody.position + direction * Speed * Time.fixedDeltaTime);
-    }
-    private void Rotate()
-    {
-        if (_goblininput.GoblinBody.transform.position.x - _goblininput.Player.transform.position.x < 0f)
-        {
-            _newrotate = true;
-            if(_rotate != _newrotate)
-            {
-                _rotate = _newrotate;
-                GoblinRotate.OnRotateRight.Invoke();
-            }
-
-        }
-        else
-        {
-            _newrotate = false;
-            if (_rotate != _newrotate)
-            {
-                _rotate = _newrotate;
-                GoblinRotate.OnRotateLeft.Invoke();
-            }
-        }
     }
 }
