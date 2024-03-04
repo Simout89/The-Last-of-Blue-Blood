fd908927560dcfe4c1b7beab4d51838cb2ca8c7e
Sim_out <alik2006sootkill@gmail.com>
2024-03-04 14:44:49 +0700

    Fix jump

--- Assets/Script/Player/PlayerInput.cs
+++ Assets/Script/Player/PlayerInput.cs
@@ -11,14 +11,15 @@ public class PlayerInput : MonoBehaviour
     public bool HeightJump { get; private set; }
     public static UnityEvent OnJump = new UnityEvent();
     public static UnityEvent OnHeightJump = new UnityEvent();
+
     public static UnityEvent<bool> OnSquat = new UnityEvent<bool>();
     private void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
         Jump = Input.GetButtonDown("Jump");
-        if(Jump) OnJump.Invoke();
+        if(Jump && !HeightJump) OnJump.Invoke();
         HeightJump = Input.GetButtonDown("HeightJump");
-        if (HeightJump) OnHeightJump.Invoke();
+        if (HeightJump && !Jump) OnHeightJump.Invoke();
         if(Input.GetButtonDown("Squat"))
             OnSquat.Invoke(true);
         if (Input.GetButtonUp("Squat"))
--- Assets/Script/Player/PlayerJump.cs
+++ Assets/Script/Player/PlayerJump.cs
@@ -7,22 +7,29 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private float JumpForce = 1f;
     private Rigidbody _rb;
     private bool _isground = false;
+    private bool _isjumpdelay = true;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         PlayerInput.OnJump.AddListener(HandleJump);
         PlayerCheckGround.IsGround.AddListener(HandeIsGround);
+        PlayerJumpDelay.OnJumpDelayOutput.AddListener(HandleJumpDelayOutput);
     }
 
     private void HandleJump()
     {
-        if(_isground)
+        if(_isground && _isjumpdelay)
         {
+            PlayerJumpDelay.OnJumpDelayInput.Invoke();
             _rb.AddForce(Vector3.up * JumpForce, ForceMode.VelocityChange);
         }
     }
     private void HandeIsGround(bool flag)
     {
         _isground = flag;
+    }
+    private void HandleJumpDelayOutput(bool flag)
+    {
+        _isjumpdelay = flag;
     }
 }
