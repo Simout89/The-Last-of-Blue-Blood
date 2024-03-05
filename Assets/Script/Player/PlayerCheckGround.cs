8da2d8f13aaf755b9c5ee81a02b50f223b44cb7f
Sim_out <alik2006sootkill@gmail.com>
2024-03-06 01:27:24 +0700

    room

--- Assets/Script/Player/PlayerCheckGround.cs
+++ Assets/Script/Player/PlayerCheckGround.cs
@@ -8,13 +8,12 @@ using UnityEngine.Experimental.GlobalIllumination;
 public class PlayerCheckGround : MonoBehaviour
 {
     public static UnityEvent<bool> IsGround = new UnityEvent<bool>();
-
-    private void OnTriggerEnter(Collider other)
-    {
-        IsGround.Invoke(true);
-    }
     private void OnTriggerExit(Collider other)
     {
         IsGround.Invoke(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        IsGround.Invoke(true);
     }
 }
